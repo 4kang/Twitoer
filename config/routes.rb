90c9e900bed7b84f1c6af7b72025c283d418ce87
4kang <106650699+4kang@users.noreply.github.com>
2023-01-05 12:22:20 +0000

    Add Favorite

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get '/' => 'homes#top'
   get '/homes/about' => 'homes#about', as: 'about'
 
-  resources :tweets, only: [:new, :create, :index, :show, :destroy] do
+  resources :tweets, only: [:new, :create, :show, :destroy] do
    resource :favorites, only: [:create, :destroy]
   end
+  get 'home' => 'tweets#index'
+
 
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
